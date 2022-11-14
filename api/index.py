eb318c2ac13625102e990c8a4206dc0463082c72
Caspar Lant <caspar@nyu.edu>
2022-11-13 23:34:20 -0500

    change root page

--- api/index.py
+++ api/index.py
@@ -143,7 +143,7 @@ def index():
   # for example, the below file reads template/index.html
   #
   # return render_template("index.html", **context)
-  return render_template("index.html")
+  return render_template("index2.html")
 
  
 #
