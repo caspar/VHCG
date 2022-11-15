df7ac3a60ac52bb5059c1299b2eb677eec2b94fa
Caspar Lant <caspar@nyu.edu>
2022-11-14 21:06:56 -0500

    cosmetic changes, mostly

--- api/index.py
+++ api/index.py
@@ -1,23 +1,11 @@
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, render_template, g, redirect, Response, flash
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
-
-   # XXX: The Database URI should be in the format of: 
-#
-#     postgresql://USER:PASSWORD@<IP_OF_POSTGRE_SQL_SERVER>/<DB_NAME>
-#
-# For example, if you had username ewu2493, password foobar, then the following line would be:
-#
-#     DATABASEURI = "postgresql://ewu2493:foobar@<IP_OF_POSTGRE_SQL_SERVER>/postgres"
-#
-# For your convenience, we already set it to the class database
-
-# Use the DB credentials you received by e-mail
 DB_USER = *****
 DB_PASSWORD = ******
 
@@ -260,10 +248,9 @@ def add_to_waitlist():
   g.conn.execute(text(cmd));
   return redirect('/plot_waitlist')
 
-@app.route('/clone')
-def clone():
+@app.route('/add_new_user', methods=['POST'])
+def add_new_user():
   print(request.args)
-  return render_template("index2.html")
 
 
 # Example of adding new data to the database
@@ -293,8 +280,6 @@ def check_login():
 
   print("CHECKING LOGIN", flush=True)
   for result in cursor:
-    
-  
     print(result)
     if result[3] == email and result[4] == phone:
       print("Successful login :",result)
